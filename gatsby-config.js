b64856a7c0445fa762df8acb88681bd5efdd07b6
yay4h <yayah.aic@gmail.com>
2025-06-16 17:54:41 +0700

    updateeeee5

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: "Hijab Girll",
     description:"This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
     author: "Yayah Paujiah",
-    keyworsd: "Hijab Girl adalah pilihan tepat bagi wanita Muslimah yang ingin mengekspresikan keindahan dalam kesederhanaan",
+    keywords: "Hijab Girl adalah pilihan tepat bagi wanita Muslimah yang ingin mengekspresikan keindahan dalam kesederhanaan",
     url:"https://hijabgirlshop.my.id/",  
   },
   plugins: [
